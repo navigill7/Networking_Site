ce1c51d5480555cd729d61cbe3d20c4906fe89eb
navigill7 <nnavigill784@gmail.com>
2024-03-16 16:30:13 +0530

    adding sidebar menu

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,99 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import {} from "@mui/material"
+import { Box, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
+import React, { useState } from 'react'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
+import EventIcon from '@mui/icons-material/Event';
+import HomeIcon from '@mui/icons-material/Home';
+import GroupIcon from '@mui/icons-material/Group';
+import AccountTreeRoundedIcon from '@mui/icons-material/AccountTreeRounded';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import SettingsIcon from '@mui/icons-material/Settings';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { styled } from '@mui/system';
+
+const StyledListItemIcon = styled(ListItemIcon)({
+  minWidth: 'auto',
+  marginRight: '16px',
+});
+
 const Sidebar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // You can implement the logic for toggling dark mode here
+  };
+
   return (
-    <Box bgcolor="blueviolet" flex={1} p={2} sx={{
+    <Box flex={1} p={2} sx={{
         display: {
             xs: "none",
             sm: "block"
         }
-    }}>Sidebar</Box>
+    }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='#' >
+            <StyledListItemIcon>
+              <HomeIcon/>
+            </StyledListItemIcon>
+            <ListItemText primary='Homepage'/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='#' >
+            <StyledListItemIcon>
+              <AccountTreeRoundedIcon/>
+            </StyledListItemIcon>
+            <ListItemText primary='Projects'/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='#' >
+            <StyledListItemIcon>
+              <EventIcon/>
+            </StyledListItemIcon>
+            <ListItemText primary='Events'/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='#' >
+            <StyledListItemIcon>
+              <GroupsRoundedIcon/>
+            </StyledListItemIcon>
+            <ListItemText primary='clubs'/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='#' >
+            <StyledListItemIcon>
+              <PeopleAltIcon/>
+            </StyledListItemIcon>
+            <ListItemText primary='Alumni'/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='#' >
+            <StyledListItemIcon>
+              <SettingsIcon/>
+            </StyledListItemIcon>
+            <ListItemText primary='Settings'/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={toggleDarkMode}>
+            <StyledListItemIcon>
+              {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
+            </StyledListItemIcon>
+            <ListItemText primary={darkMode ? 'Light' : 'Dark'} />
+            <Switch checked={darkMode} onChange={toggleDarkMode} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
   )
 }
 
-export default Sidebar
+export default Sidebar;
